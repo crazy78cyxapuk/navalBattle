da517b734806bfcd87b06ad20e697df191856bfd
crazy_cyxapuk <olezhki.net@gmail.com>
2019-11-24 22:52:22 +0300

    сделана только одна расстановка своих кораблей

---
+++ Assets/scripts/arrangement.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrangement : MonoBehaviour
+{
+    public static int[,] arr1 = new int[20, 20];
+    public static bool finishArrang;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        finishArrang = false;
+
+        for (int i = 0; i < 20; i++)
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                arr1[i, j] = 0;
+            }
+        }
+
+        goArrangement1();
+
+        //fillCage();
+
+        //for(int i = 0; i < 20; i++)
+        //{
+        //    for(int j=0; j<20; j++)
+        //    {
+        //        
+        //    }
+        //}
+    }
+
+    
+    public void goArrangement1()
+    {
+        //строка х столбец
+
+        for(int i = 0; i <= 3; i++)
+        {
+            arr1[0, i] = -1; //1
+        }
+
+        for (int i = 5; i <= 8; i++)
+        {
+            arr1[0, i] = -1; //2
+        }
+
+        for (int i = 10; i <= 13; i++)
+        {
+            arr1[0, i] = -1; //3
+        }
+
+        for (int i = 15; i <= 17; i++)
+        {
+            arr1[0, i] = -1; //4
+        }
+
+        for (int i = 2; i <= 6; i++)
+        {
+            arr1[i, 0] = -1; //5
+        }
+
+        for (int i = 8; i <= 12; i++)
+        {
+            arr1[i, 0] = -1; //6
+        }
+
+        for (int i = 14; i <= 19; i++)
+        {
+            arr1[i, 0] = -1; //7
+        }
+
+        for (int i = 9; i <= 10; i++)
+        {
+            arr1[i, 2] = -1; //8
+        }
+
+        for (int i = 12; i <= 13; i++)
+        {
+            arr1[i, 2] = -1; //9
+        }
+
+        for (int i = 15; i <= 16; i++)
+        {
+            arr1[i, 2] = -1; //10
+        }
+
+        for (int i = 18; i <= 19; i++)
+        {
+            arr1[i, 2] = -1; //11
+        }
+
+        for (int i = 0; i <= 2; i++)
+        {
+            arr1[i, 19] = -1; //12
+        }
+
+        for (int i = 4; i <= 6; i++)
+        {
+            arr1[i, 19] = -1; //13
+        }
+
+        for (int i = 8; i <= 10; i++)
+        {
+            arr1[i, 19] = -1; //14
+        }
+
+        for (int i = 12; i <= 14; i++)
+        {
+            arr1[i, 19] = -1; //15
+        }
+
+        for (int i = 16; i <= 18; i++)
+        {
+            arr1[i, 19] = -1; //16
+        }
+
+        for (int i = 4; i <= 5; i++)
+        {
+            arr1[19, i] = -1; //17
+        }
+
+        for (int i = 7; i <= 8; i++)
+        {
+            arr1[19, i] = -1; //18
+        }
+
+        for (int i = 10; i <= 11; i++)
+        {
+            arr1[19, i] = -1; //19
+        }
+
+        for (int i = 13; i <= 14; i++)
+        {
+            arr1[19, i] = -1; //20
+        }
+
+        for (int i = 16; i <= 17; i++)
+        {
+            arr1[19, i] = -1; //21
+        }
+
+        arr1[2, 4] = -1;
+        arr1[2, 6] = -1;
+        arr1[2, 8] = -1;
+        arr1[2, 10] = -1;
+        arr1[2, 12] = -1;
+        arr1[2, 14] = -1;
+        arr1[2, 16] = -1;
+
+        arr1[4, 17] = -1;
+        arr1[6, 17] = -1;
+        arr1[8, 17] = -1;
+        arr1[10, 17] = -1;
+        arr1[12, 17] = -1;
+
+        arr1[3, 2] = -1;
+        arr1[5, 2] = -1;
+        arr1[7, 2] = -1;
+
+        finishArrang = true;
+    }
+
+    
+}
---
+++ Assets/scripts/myPlane.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myPlane : MonoBehaviour
+{
+
+    public GameObject myCage;
+    public static GameObject[,] plane = new GameObject[20, 20];
+    //private GameObject currentCage;
+    //private int id;
+    private float posX, posY;
+    private float startPosX = -8;
+    private float startPosY = 4.8f;
+    public Sprite ship;
+
+    private void Start()
+    {
+        posX = .3f;
+        posY = .3f;
+
+        for (int i=0; i < 20; i++)
+        {
+            for(int j=0; j < 20; j++)
+            {
+                plane[i, j] = Instantiate(myCage, new Vector3(startPosX, startPosY, 0), Quaternion.identity); //as GameObject;
+                startPosX += posX;
+                plane[i, j].name = i.ToString() + " " + j.ToString();
+                plane[i, j].transform.parent = transform;
+            }
+            startPosY -= posY;
+            startPosX = -8;
+        }
+
+        for (int i = 0; i < 20; i++)
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                //plane[i, j].GetComponent<Renderer>().material.color = Color.red;
+            }
+        }
+    }
+
+
+    private void fillCage()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                if (arrangement.arr1[i, j] == -1)
+                {
+                    //plane[i, j].GetComponent<Renderer>().material.color = Color.red;
+                    plane[i, j].GetComponent<SpriteRenderer>().sprite = ship;
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (arrangement.finishArrang)
+        {
+            fillCage();
+        }
+    }
+
+}
