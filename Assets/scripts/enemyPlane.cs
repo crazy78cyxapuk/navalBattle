8c93ee4e4917a7aa9ef8aa62e00ef75b7252e630
crazy_cyxapuk <olezhki.net@gmail.com>
2019-12-17 02:21:25 +0300

    добавлена спираль в две стороны; добавлена обводка мертвых кораблей;

--- Assets/scripts/enemyPlane.cs
+++ Assets/scripts/enemyPlane.cs
@@ -43,8 +43,67 @@ public class enemyPlane : MonoBehaviour
             startPosX = 2;
         }
 
+        
+        for (int i = 0; i < 20; i++)
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                enemyArr[i, j] = -1;
+            }
+        }
+
+        //!!!!
+        for (int i = 0; i <= 5; i++)//test ship 1
+        {
+            enemyArr[i, 4] = 1;
+        }
+
+        for (int j = 0; j <= 5; j++) //test ship 2
+        {
+            enemyArr[14, j] = 1;
+        }
+
+        enemyArr[19, 19] = 1; //test ship 3
+
+        //!!!!
+
         fillEnemyShip();
+
+
+
     }
+
+    //public void attackInShip()
+    //{//атака спиралью по часовой
+    //    int n = 19;
+    //    int k = 0;
+    //    for (int i = 0; i < 10; i++)
+    //    {
+
+    //        for (int i1 = k; i1 < n; i1++)
+    //        {
+    //            enemyArr[k, i1] = 1;
+    //        }
+
+    //        for (int i2 = k; i2 < n; i2++)
+    //        {
+    //            enemyArr[i2, n] = 1;
+    //        }
+
+    //        for (int i3 = n; i3 >= k; i3--)
+    //        {
+    //            enemyArr[n, i3] = 1;
+    //        }
+
+    //        for (int i4 = n; i4 >= k + 1; i4--)
+    //        {
+    //            enemyArr[i4, k] = 1;
+    //        }
+
+    //        n -= 1;
+    //        k += 1;
+    //    }
+    //}
 
     public static void fillEnemyShip()
     {
@@ -52,17 +111,17 @@ public class enemyPlane : MonoBehaviour
         {
             for(int j=0; j < 20; j++)
             {
-                if (enemyArr[i, j] == 1)
+                if (enemyArr[i, j] == 1) //ранен\убит
                 {
                     GameObject obj = GameObject.Find(i.ToString() + " " + j.ToString() + " enemy");
                     obj.GetComponent<Renderer>().material.color = Color.red;
                 }
-                if (enemyArr[i, j] == -1)
+                if (enemyArr[i, j] == -1) //статус до выстрела
                 {
                     GameObject obj = GameObject.Find(i.ToString() + " " + j.ToString() + " enemy");
                     obj.GetComponent<Renderer>().material.color = Color.blue;
                 }
-                if (enemyArr[i, j] == 0)
+                if (enemyArr[i, j] == 0) //пусто
                 {
                     GameObject obj = GameObject.Find(i.ToString() + " " + j.ToString() + " enemy");
                     obj.GetComponent<Renderer>().material.color = Color.white;
--- Assets/scripts/killShip.cs
+++ Assets/scripts/killShip.cs
@@ -14,8 +14,57 @@ public class killShip : MonoBehaviour
 
     public void pisatZdesDima()
     {
-        //tyt kod pisat'
-        enemyPlane.enemyArr[1, 1] = 0;
+        //обводит корабли вокруг ячейки застрелленым индексом
+        for (int i = 0; i < 20; i++)
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                if (enemyPlane.enemyArr[i, j] == 1)
+                {
+                    if (i + 1 <= 19 && enemyPlane.enemyArr[i + 1, j] != 1) //1
+                    {
+                        enemyPlane.enemyArr[i + 1, j] = 0;
+                    }
+
+                    if (i + 1 <= 19 && j + 1 <= 19 && enemyPlane.enemyArr[i + 1, j + 1] != 1) //2
+                    {
+                        enemyPlane.enemyArr[i + 1, j + 1] = 0;
+                    }
+
+                    if (j + 1 <= 19 && enemyPlane.enemyArr[i, j + 1] != 1) //3
+                    {
+                        enemyPlane.enemyArr[i, j + 1] = 0;
+                    }
+
+                    if (j + 1 <= 19 && i - 1 >= 0 && enemyPlane.enemyArr[i - 1, j + 1] != 1) //4
+                    {
+                        enemyPlane.enemyArr[i - 1, j + 1] = 0;
+                    }
+
+                    if (i - 1 >= 0 && enemyPlane.enemyArr[i - 1, j] != 1) //5
+                    {
+                        enemyPlane.enemyArr[i - 1, j] = 0;
+                    }
+
+                    if (j - 1 >= 0 && i - 1 >= 0 && enemyPlane.enemyArr[i - 1, j - 1] != 1) //6
+                    {
+                        enemyPlane.enemyArr[i - 1, j - 1] = 0;
+                    }
+
+                    if (j - 1 >= 0 && enemyPlane.enemyArr[i, j - 1] != 1) //7
+                    {
+                        enemyPlane.enemyArr[i, j - 1] = 0;
+                    }
+
+                    if (j - 1 >= 0 && i + 1 <= 19 && enemyPlane.enemyArr[i + 1, j - 1] != 1) //8
+                    {
+                        enemyPlane.enemyArr[i + 1, j - 1] = 0;
+                    }
+
+                }
+            }
+        }
+
         enemyPlane.fillEnemyShip();
     }
 }
